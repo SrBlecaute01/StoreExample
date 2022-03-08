8a18b45a572b1412f7b5bb451e9ff1de56377f2f
SrBlecaute01 <josearnaldoma18k@gmail.com>
2022-03-08 00:37:15 -0300

    :bug: Fixed user update dto.

--- src/main/java/br/com/blecaute/store/dto/UserUpdateDTO.java
+++ src/main/java/br/com/blecaute/store/dto/UserUpdateDTO.java
@@ -11,15 +11,8 @@ import javax.validation.constraints.*;
 @AllArgsConstructor
 public class UserUpdateDTO {
 
-    @Null
-    @NotBlank
     private String name;
-
-    @Null
-    @Email
     private String email;
-
-    @Null
-    @Size(min = 3, max = 16, message = "The password must be between 3 and 16 characters long")
     private String password;
+
 }
